90cfd0afb33748f2ad8f5b1d86c45477f1321731
Daniel Hammond <viztheman@protonmail.com>
2019-04-29 16:08:07 -0500

    Added checks for signature. Reduced hmac sig to only using method, URL, and Date since it's, y'know, SIMPLE Hmac. :P

---
+++ test/strategy_createHash_test.js
@@ -0,0 +1,22 @@
+const chai = require('chai');
+const expect = chai.expect;
+const Strategy = require('../lib/strategy');
+
+const PRIVATE_KEY = '5b1b2c1c-812e-429d-916c-cee953b4a0e5';
+
+const REQ = {
+    method: 'GET',
+    originalUrl: '/test/url?a=1',
+    date: 'Mon, 29 Apr 2019 20:36:18 GMT'
+};
+const EXPECTED_HMAC = 'ce1f7165f74a99ce48727e535852698623daaaad';
+
+describe('Strategy', () => {
+    describe('createHash', () => {
+        it('should generate expected HMAC', () => {
+            let strategy = new Strategy(() => {});
+            let hash = strategy.createHash(REQ, PRIVATE_KEY);
+            expect(hash).to.equal(EXPECTED_HMAC);
+        });
+    });
+});
--- test/strategy_createSig_test.js
+++ test/strategy_createSig_test.js
@@ -2,66 +2,20 @@ const chai = require('chai');
 const expect = chai.expect;
 const Strategy = require('../lib/strategy');
 
-const TEST_URL = '/test/items?abc=123';
+const REQ = {
+    method: 'GET',
+    originalUrl: '/testing/test?a=1&b=2',
+    date: 'Fri, 02 Feb 1996 03:04:05 GMT'
+};
 
-function createSigBody(req, isJson) {
-    let lines = [req.method, req.originalUrl, req.date];
-
-    if (req.body)
-        lines.push(isJson ? JSON.stringify(req.body) : req.body);
-
-    return lines.join('\n');
-}
+const EXPECTED_SIG = [REQ.method, REQ.originalUrl, REQ.date].join('\n');
 
 describe('Strategy', () => {
     describe('createSig', () => {
-        it('should use method, originalUrl, and date on GET', () => {
-            let req = {method: 'GET', originalUrl: TEST_URL, date: new Date().toUTCString()};
+        it('should create the expected signature', () => {
             let strategy = new Strategy(() => {});
-            let sig = strategy.createSig.call(strategy, req);
-
-            let expected = createSigBody(req);
-            expect(sig).to.equal(expected);
-        });
-
-        it('should use method, originalUrl, and date on DELETE', () => {
-            let req = {method: 'DELETE', originalUrl: TEST_URL, date: new Date().toUTCString()};
-            let strategy = new Strategy(() => {});
-            let sig = strategy.createSig.call(strategy, req);
-
-            let expected = createSigBody(req);
-            expect(sig).to.equal(expected);
-        });
-
-        it('should use method, originalUrl, date, and urlencoded body on postback', () => {
-            let req = {
-                method: 'POST',
-                originalUrl: TEST_URL,
-                date: new Date().toUTCString(),
-                body: 'a=1&b=2&c=3',
-                headers: {}
-            };
-            let strategy = new Strategy(() => {});
-            let sig = strategy.createSig.call(strategy, req);
-
-            let expected = createSigBody(req);
-            expect(sig).to.equal(expected);
-        });
-
-        it('should use method, originalUrl, date, and json body on json postback', () => {
-            let jsonPayload = {a: 1, b: 2, c: 3};
-            let req = {
-                method: 'POST',
-                originalUrl: TEST_URL,
-                date: new Date().toUTCString(),
-                body: jsonPayload,
-                headers: {'content-type': 'application/json'}
-            };
-            let strategy = new Strategy(() => {});
-            let sig = strategy.createSig.call(strategy, req);
-
-            let expected = createSigBody(req, true);
-            expect(sig).to.equal(expected);
+            let sig = strategy.createSig.call(this, REQ);
+            expect(sig).to.equal(EXPECTED_SIG);
         });
     });
 });
