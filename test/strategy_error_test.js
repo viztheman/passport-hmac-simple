9a24abd3583e5d54812fbb4b33088772b83004db
Daniel Hammond <viztheman@protonmail.com>
2019-04-30 11:01:36 -0500

    Error-case unit tests working. Fixes to hash extraction on strategy.

---
+++ test/strategy_error_test.js
@@ -0,0 +1,59 @@
+const chai = require('chai');
+const passport = require('chai-passport-strategy');
+const Strategy = require('../lib/strategy');
+
+const ERROR_TEXT = 'boom';
+const AUTH_HEADER = 'restify-todo IjA3MDVkNWEyLWZhZWYtNDMwMi1iMjU3LThkYzliZjUyMjdhOTpZVGswWVRobVpUVmpZMkl4T1dKaE5qRmpOR013T0RjelpETTVNV1U1T0RjNU9ESm1ZbUprTXc9PSci';
+
+const expect = chai.expect;
+chai.use(passport);
+
+describe('Strategy', () => {
+    describe('authenticate', () => {
+        describe('Graceful errors', () => {
+            var err;
+
+            const strategy = new Strategy(function(pk, done) {
+                return done(new Error(ERROR_TEXT));
+            });
+
+            before(function(done) {
+                chai.passport.use(strategy)
+                    .error(function(e) {
+                        err = e;
+                        done();
+                    })
+                    .req(req => req.headers.authorization = AUTH_HEADER)
+                    .authenticate();
+            });
+
+            it('should call this.error', () => {
+                expect(err).to.be.an.instanceOf(Error);
+                expect(err.message).to.equal(ERROR_TEXT);
+            });
+        });
+        
+        describe('Ungraceful error', () => {
+            var err;
+
+            const strategy = new Strategy(function(pk, done) {
+                throw new Error(ERROR_TEXT);
+            });
+
+            before(function(done) {
+                chai.passport.use(strategy)
+                    .error(function(e) {
+                        err = e;
+                        done();
+                    })
+                    .req(req => req.headers.authorization = AUTH_HEADER)
+                    .authenticate();
+            });
+
+            it('should call this.error', () => {
+                expect(err).to.be.an.instanceOf(Error);
+                expect(err.message).to.equal(ERROR_TEXT);
+            });
+        });
+    });
+});
